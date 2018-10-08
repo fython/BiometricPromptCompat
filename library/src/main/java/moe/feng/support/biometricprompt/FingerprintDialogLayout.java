c4654e0334d3e18b284513a3c8eae306e4fdac74
drakeet <drakeet@drakeet.com>
2018-10-08 18:09:56 +0800

    Updated indentation style to be consistent with others

--- library/src/main/java/moe/feng/support/biometricprompt/FingerprintDialogLayout.java
+++ library/src/main/java/moe/feng/support/biometricprompt/FingerprintDialogLayout.java
@@ -8,36 +8,36 @@ import android.util.AttributeSet;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-@RestrictTo({ RestrictTo.Scope.LIBRARY })
+@RestrictTo({RestrictTo.Scope.LIBRARY})
 public class FingerprintDialogLayout extends LinearLayout {
 
-  private FingerprintIconView iconView;
-  private TextView statusView;
+    private FingerprintIconView iconView;
+    private TextView statusView;
 
-  public FingerprintDialogLayout(Context context) {
-    super(context);
-  }
+    public FingerprintDialogLayout(Context context) {
+        super(context);
+    }
 
-  public FingerprintDialogLayout(Context context, @Nullable AttributeSet attrs) {
-    super(context, attrs);
-  }
+    public FingerprintDialogLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-  public FingerprintDialogLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-  }
+    public FingerprintDialogLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-  @Override
-  protected void onFinishInflate() {
-    super.onFinishInflate();
-    iconView = findViewById(R.id.fingerprint_icon);
-    statusView = findViewById(R.id.status);
-  }
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        iconView = findViewById(R.id.fingerprint_icon);
+        statusView = findViewById(R.id.status);
+    }
 
-  @Override
-  protected void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    int newMargin = getResources().getDimensionPixelSize(R.dimen.fingerprint_status_layout_margin_vertical);
-    ((MarginLayoutParams) iconView.getLayoutParams()).topMargin = newMargin;
-    ((MarginLayoutParams) statusView.getLayoutParams()).bottomMargin = newMargin;
-  }
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        int newMargin = getResources().getDimensionPixelSize(R.dimen.fingerprint_status_layout_margin_vertical);
+        ((MarginLayoutParams) iconView.getLayoutParams()).topMargin = newMargin;
+        ((MarginLayoutParams) statusView.getLayoutParams()).bottomMargin = newMargin;
+    }
 }
